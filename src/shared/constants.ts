91e1c825c0cdf0fb76a53bb9318b8ebb2b797c3d
Van Ivan <159064779+IvanBadProger@users.noreply.github.com>
2025-03-24 14:51:49 +0700

    Update constants.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:3000"
-export const BASE_URL = "https://your-vercel-app.vercel.app/api"
+export const BASE_URL = "/api"
 
 export const API_ENDPOINTS = {
   PRODUCTS: {
